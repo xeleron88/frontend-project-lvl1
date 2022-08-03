533d27cb168b58b3338abab0a59a328291320449
xeleron <xelseo@gmail.com>
2022-08-03 16:19:34 +0300

    Edited Eslint config

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 
 const cli = () => {
   const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`)
-  return;
+  console.log(`Hello, ${name}!`);
 };
 export default cli;
